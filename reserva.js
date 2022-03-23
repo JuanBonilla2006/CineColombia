300331f0103ca574cba189d1ce103358b02ee28f
jaja <jaja@gmail.com>
2022-03-23 17:19:08 -0500

    cucha

--- reserva.js
+++ reserva.js
@@ -1,8 +1,49 @@
 console.log("hola")
 let contenedor=document.getElementById("contenedor")
 console.log(contenedor)
+
+let bandera=true
+
 contenedor.addEventListener("click",function(evento){
-    if(evento.target.classList.contains="w-100"){
-        evento.target.src="img/rojo.png"
+
+
+    if(bandera==true){
+        
+        if(evento.target.classList.contains="w-100"){
+            evento.target.src="img/rojo.png"
+            bandera=false
+        }
+
+    }else{
+
+        if(evento.target.classList.contains="w-100"){
+            evento.target.src="img/cinema-chair.png"
+            bandera=true
+        }
+
+    }
+
+
+})
+
+
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+
+    evento.preventDefault()
+
+    let cantidad=document.getElementById("cantidad").value
+    let asiento=document.getElementById("asiento").value
+    let pelicula=document.getElementById("pelicula").value
+
+    if(asiento=="general"){
+        let precio=18050
+        let total=precio*cantidad
+        alert("Sus entradas valen"+total)
+    
+    }else{
+        let precio=26050
+        let total=precio*cantidad
+        alert("Sus entradas valen"+total)
     }
 })
